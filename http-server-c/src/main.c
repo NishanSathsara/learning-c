b232169ddb8bfe6a4e9e36283c40bb425cd4c4a0
NishanSathsara <nishanksathsara@gmail.com>
2026-06-19 14:17:58 +0530

    Fix nested git repository and track http-server-c files

---
+++ http-server-c/src/main.c
@@ -0,0 +1,103 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netinet/ip.h>
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
+
+int main() {
+	// Disable output buffering
+	setbuf(stdout, NULL);
+ 	setbuf(stderr, NULL);
+
+	// You can use print statements as follows for debugging, they'll be visible when running tests.
+	printf("Logs from your program will appear here!\n");
+
+	// TODO: Uncomment the code below to pass the first stage
+	
+	 int server_fd, client_addr_len;
+	 struct sockaddr_in client_addr;
+	
+	 server_fd = socket(AF_INET, SOCK_STREAM, 0);
+	 if (server_fd == -1) {
+	 	printf("Socket creation failed: %s...\n", strerror(errno));
+	 	return 1;
+	 }
+	
+	 // Since the tester restarts your program quite often, setting SO_REUSEADDR
+	 // ensures that we don't run into 'Address already in use' errors
+	 int reuse = 1;
+	 if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0) {
+	 	printf("SO_REUSEADDR failed: %s \n", strerror(errno));
+	 	return 1;
+	 }
+	
+	 struct sockaddr_in serv_addr = { 
+		 .sin_family = AF_INET ,
+	 	 .sin_port = htons(4221),
+	 	 .sin_addr = { htonl(INADDR_ANY) },
+	};
+	
+	 if (bind(server_fd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) != 0) {
+	 	printf("Bind failed: %s \n", strerror(errno));
+	 	return 1;
+	 }
+	
+	 int connection_backlog = 5;
+	 if (listen(server_fd, connection_backlog) != 0) {
+	 	printf("Listen failed: %s \n", strerror(errno));
+	 	return 1;
+	 }
+	
+	 printf("Waiting for a client to connect...\n");
+	 
+	 client_addr_len = sizeof(client_addr);
+	
+	 int client_fd = accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t*) &client_addr_len);
+	 
+	 if (client_fd == -1){
+		printf("Accept falied: %s", strerror(errno));
+		close(client_fd);
+
+		return 1;
+	 }
+
+	 printf("Client connected\n");
+
+	 char request_buffer[1024];
+	 
+	 int bytes_received = recv(client_fd, request_buffer, sizeof(request_buffer)-1, 0);
+
+	 if (bytes_received < 0){
+
+	 	printf("Read failed: %s/n", strerror(errno));
+		close(client_fd);
+	 	close(server_fd);
+		return 1;
+	 }
+
+	 request_buffer[bytes_received] = '\0';
+
+	 char request_method [64];
+	 char request_target [1024];
+
+	 sscanf(request_buffer, "%s %s", request_method, request_target);
+
+	 if(strcmp(request_target, "/")==0){
+	 	char *res = "HTTP/1.1 200 OK\r\n\r\n";
+	 	send(client_fd, res , strlen(res), 0);
+	 }else{
+	 	char *res = "HTTP/1.1 404 Not Found\r\n\r\n";
+	 	send(client_fd, res , strlen(res), 0);
+	 }
+
+
+
+
+	 close(client_fd);
+	 close(server_fd);
+
+	return 0;
+}
